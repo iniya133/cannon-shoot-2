e226ff4d0a93f9f9981a464d75a9f8fb386a709e
iniya133 <56218662+iniya133@users.noreply.github.com>
2020-02-10 19:35:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+//This is the cannonball game project 26
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, cannon;
+var angle = 0;
+var ball1, ball2, ball3;
+
+function setup() {
+  createCanvas(900, 550);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(450, 530, 900, 50); 
+  cannon = new Cannon(75,height-125,60,30);
+  ball1 = new Ball(600, 250, 20);
+  ball2 = new Ball(700, 250, 20);
+  ball3 = new Ball(500, 250, 20);
+}
+
+function draw() {
+  background(255);
+  Engine.update(engine);
+  //Display
+  ground.display();
+  cannon.display();
+  ball1.display();
+  ball2.display();
+  ball3.display();
+}
